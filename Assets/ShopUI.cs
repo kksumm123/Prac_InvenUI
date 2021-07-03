72b26c9f7f0aa8fc547fe875632c328d16d9ca7a
kksumm123 <kksumm123@gmail.com>
2021-07-03 18:45:07 +0900

    ShopUI작성완료

--- Assets/ShopUI.cs
+++ Assets/ShopUI.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ShopUI : MonoBehaviour
 {
     // ItemDataManager의 ItemDataList 내용 가져와서 Shop내에 진열시켜줘야함
-    GameObject itemBaseGo;
+    ShopItem itemBase;
     private void Start()
     {
         ShopItemUpdate();
@@ -13,13 +13,14 @@ public class ShopUI : MonoBehaviour
     void ShopItemUpdate()
     {
         //itemBaseTr = transform.Find("Viewport/Content/ItemBase");
-        itemBaseGo = (GameObject)FindObjectOfType(typeof(ShopItem), true);
-
-        Debug.Assert(itemBaseGo != null, "itembase 탐색 실패");
+        itemBase = (ShopItem)FindObjectOfType(typeof(ShopItem), true);
+        itemBase.gameObject.SetActive(true);
+        Debug.Assert(itemBase != null, "itembase 탐색 실패");
         foreach (var item in ItemDataManager.instance.itemDataList)
         {
-            var newItem = Instantiate(itemBaseGo, itemBaseGo.transform.parent);
-            newItem.
+            var newItem = Instantiate(itemBase, itemBase.transform.parent);
+            newItem.Init(item);
         }
+        itemBase.gameObject.SetActive(false);
     }
 }
