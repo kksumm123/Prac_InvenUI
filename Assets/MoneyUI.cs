73aeaf45853d4270c14f56025696f54f80046613
kksumm123 <kksumm123@gmail.com>
2021-07-03 21:22:24 +0900

    MoneyUI 구현완료

--- Assets/MoneyUI.cs
+++ Assets/MoneyUI.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoneyUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 현재 재화정보를 가져와야함
+    public static MoneyUI instance;
+    Text goldText;
+    Text diaText;
+    private void Awake()
     {
-        
+        instance = this;
+        goldText = transform.Find("Gold/Value").GetComponent<Text>();
+        diaText = transform.Find("Dia/Value").GetComponent<Text>();
+    }
+    private void Start()
+    {
+        RefreshUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RefreshUI()
     {
-        
+        goldText.text = UserData.instance.Gold.ToString();
+        diaText.text = UserData.instance.Dia.ToString();
     }
 }
