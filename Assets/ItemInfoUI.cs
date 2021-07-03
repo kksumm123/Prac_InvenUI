e08c116fc300cb0be4ad7603e01e5ba5af4df031
kksumm123 <kksumm123@gmail.com>
2021-07-03 19:49:27 +0900

    ItemInfoUI 작성완료

--- Assets/ItemInfoUI.cs
+++ Assets/ItemInfoUI.cs
@@ -1,9 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemInfoUI : MonoBehaviour
 {
     // Shop, Iven에서 Item이 클릭되면 표시해줘야함
+    public static ItemInfoUI instance;
+    private void Awake()
+    {
+        instance = this;
+    }
 
+    Image icon;
+    Text itemName;
+    Text desciption;
+    GameObject shopBtn;
+    GameObject invenBtn;
+
+    private void Start()
+    {
+        icon = transform.Find("Icon").GetComponent<Image>();
+        itemName = transform.Find("ItemName").GetComponent<Text>();
+        desciption = transform.Find("Description").GetComponent<Text>();
+        shopBtn = transform.Find("ShopBtn").gameObject;
+        invenBtn = transform.Find("InvenBtn").gameObject;
+        shopBtn.SetActive(false);
+        invenBtn.SetActive(false);
+    }
+
+    public void ShowShopItemInfo(ItemDataInfo itemDataInfo)
+    {
+        shopBtn.SetActive(true);
+        shopBtn.SetActive(false);
+        SetItemInfo(itemDataInfo);
+    }
+
+    void SetItemInfo(ItemDataInfo itemDataInfo)
+    {
+        icon.sprite = itemDataInfo.icon;
+        itemName.text = itemDataInfo.name;
+        desciption.text = itemDataInfo.description;
+    }
 }
--- Assets/ShopItem.cs
+++ Assets/ShopItem.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ShopItem : MonoBehaviour
+public class ShopItem : MonoBehaviour, IPointerClickHandler
 {
     //입력받은대로 아이콘, 가격을 변경함
     public void Init(ItemDataInfo item)
@@ -11,5 +12,9 @@ public class ShopItem : MonoBehaviour
         transform.name = item.name;
         GetComponentInChildren<Image>().sprite = item.icon;
         GetComponentInChildren<Text>().text = item.buyPrice.ToString();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
     }
 }
