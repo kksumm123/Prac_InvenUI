694be6d8d5b733f43ee124ca386c376ceb6d20a2
kksumm123 <kksumm123@gmail.com>
2021-07-03 18:41:19 +0900

    ShopItem 작성 완료

--- Assets/ShopItem.cs
+++ Assets/ShopItem.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopItem : MonoBehaviour
 {
     //입력받은대로 아이콘, 가격을 변경함
-    public void Init(ItemDataInfo itemData)
+    public void Init(ItemDataInfo item)
     {
-        
+        transform.name = item.name;
+        GetComponentInChildren<Image>().sprite = item.icon;
+        GetComponentInChildren<Text>().text = item.buyPrice.ToString();
     }
 }
