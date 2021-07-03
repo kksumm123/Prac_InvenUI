ed5fc1801256f79cc012ee2147da866be0ba681d
kksumm123 <kksumm123@gmail.com>
2021-07-03 20:31:32 +0900

    userdata 싱글톤

--- Assets/UserData.cs
+++ Assets/UserData.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class UserData : MonoBehaviour
 {
+    // 유저의 재화(gold, dia), 아이템 저장해줘야함
+    public static UserData instance;
+    private void Awake()
+    {
+        instance = this;
+    }
 
 }
