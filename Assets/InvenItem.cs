1e2b5de98022adb05386d8e251b2f2af6b908560
kksumm123 <kksumm123@gmail.com>
2021-07-03 22:24:57 +0900

    InvenItem 구현중 InvenUI 구현중 ItemInfoUI 수정중

---
+++ Assets/InvenItem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InvenItem : MonoBehaviour, IPointerClickHandler
+{
+    InvenItemInfo invenItemInfo;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+            ItemInfoUI.instance.ShowInvenItem(invenItemInfo);
+    }
+    public void Init(InvenItemInfo item)
+    {
+        invenItemInfo = item;
+        ItemDataInfo itemDataInfo = invenItemInfo.GetItemDataInfo();
+        GetComponent<Image>.sprite = itemDataInfo.icon;
+
+    }
+}
--- Assets/InvenUI.cs
+++ Assets/InvenUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,5 +7,31 @@ public class InvenUI : MonoBehaviour
 {
     // 아이템 목록을 그려주면 된다
     // list 변수 하나 만들기 (UserData에 만듬)
+    public static InvenUI instanse;
+    private void Awake()
+    {
+        instanse = this;
+    }
+    [SerializeField] InvenItem itemBase;
+    void Start()
+    {
+        RefreshUI();
+    }
 
+    List<GameObject> childitemlist = new List<GameObject>();
+    public void RefreshUI()
+    {
+        childitemlist.ForEach(x => Destroy(x));
+        childitemlist.Clear();
+
+        itemBase = (InvenItem)FindObjectOfType(typeof(InvenItem), true);
+        itemBase.gameObject.SetActive(true);
+        Debug.Assert(itemBase != null, "itembase 탐색 실패");
+        foreach (var item in UserData.instance.invenItems)
+        {
+            var newItem = Instantiate(itemBase, itemBase.transform.parent);
+            newItem.Init(item);
+        }
+        itemBase.gameObject.SetActive(false);
+    }
 }
