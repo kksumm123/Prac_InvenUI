1296c9a7026baa0b8f46f05743f3374137f09f2b
kksumm123 <kksumm123@gmail.com>
2021-07-03 18:11:10 +0900

    1. icon 자료형 Image->Sprice 2. List<ItemData> 추가

--- Assets/ItemDataManager.cs
+++ Assets/ItemDataManager.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public enum ItemType
-{ 
+{
     Weapon,
     Armor,
     Potion,
 }
+[System.Serializable]
 public class ItemData
 {
     // Name, ItemID, Type, Icon, Description, BuyPrice, SellPrice
     public string name;
     public int ItemID;
     public ItemType type;
-    public Image icon;
+    public Sprite icon;
     public string description;
     public int buyPrice;
     public int sellPrice;
 }
 public class ItemDataManager : MonoBehaviour
 {
+    public List<ItemData> itemDataList = new List<ItemData>();
     public static ItemDataManager instance;
     private void Awake()
     {
